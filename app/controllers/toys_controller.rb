e013fcdc09c67705c409a2ea4a7826078766d380
Daniel Clements <drclements@liberty.edu>
2023-01-11 18:48:20 -0500

    commit tree 115968d0cdfa3059118ef3a683f1d815b5f2d4df

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,4 +1,5 @@
 class ToysController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   wrap_parameters format: []
 
   def index
@@ -7,16 +8,18 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy, status: :created
   end
 
   def destroy
+    byebug
     toy = Toy.find_by(id: params[:id])
     toy.destroy
     head :no_content
@@ -26,6 +29,10 @@ class ToysController < ApplicationController
   
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
